272972afad2e1e250b634912e3fad380f18730a6
E Cussler <cusslere@gmail.com>
2023-01-05 17:01:05 -0800

    Adjusted port to work with Heroku

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const notesDB = require('./db/db.json');
 const util = require('util');
 const uuid = require('./helpers/uuid'); 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001; 
 
 const app = express();
 
